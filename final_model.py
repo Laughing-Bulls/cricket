d1e6f614cbafc32eb5d1b48a16dcb40066a3c44e
Laughing-Bulls <asd26@njit.edu>
2022-04-17 23:24:41 -0400

    Add to github

--- final_model.py
+++ final_model.py
@@ -13,10 +13,11 @@ def predict_score(input):
     var = np.zeros((1, 4))
     print("Predict Score: The Model parameters: ")
     print(model_parameters)
+    var[0, 0] = model_parameters.iat[0, 0]
     var[0, 1] = model_parameters.iat[0, 1]
     var[0, 2] = model_parameters.iat[0, 2]
     var[0, 3] = model_parameters.iat[0, 3]
-    print("Predict Score: The Model weightings: ", var[0, 1], var[0, 2], var[0, 3])
+    print("Predict Score: The Model weightings: ", var)
     pred = input + var
     print('Final Model: The model predicts:', pred)  # Print final answer.
     return pred
